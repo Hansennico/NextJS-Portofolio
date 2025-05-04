209fc36d3c5eb76a9c2eff5f7c2a3ae8b815e19c
Hansennico <nicolashansen@gmail.com>
2025-05-05 02:39:37 +0700

    edit Project data.ts
    adding more project

--- app/projects/data.ts
+++ app/projects/data.ts
@@ -1,56 +1,39 @@
 import dummy from "@/Public/dummy.webp";
-import img1 from "@/Public/DiscordPy.webp";
+import discordPy from "@/Public/DiscordPy.webp";
+import springLogo from "@/Public/Spring_boot_logo.webp";
 
 const database = [
   {
+    name: "EndpointAPI CRUD",
+    image: springLogo,
+    description:
+      "Simple Spring Boot Web Application that provides RESTful API endpoints",
+    languages: "Java, HTML, CSS",
+    url: "https://github.com/Hansennico/EndpointAPICRUD",
+  },
+  {
+    name: "Bulletin Board",
+    image: springLogo,
+    description:
+      "Simple bulletin board web application using Spring Boot Web Framework",
+    languages: "Java, HTML, CSS",
+    url: "https://github.com/Hansennico/Bulletin-Implementation/tree/main",
+  },
+  {
+    name: "WTIX",
+    image: springLogo,
+    description:
+      "A simple web application for a movie ticketing system using Spring Boot Web Framework",
+    languages: "Java, HTML, CSS",
+    url: "",
+  },
+  {
     name: "Discord Bot Py",
-    image: img1,
+    image: discordPy,
     description:
       "A simple discord bot using python, that can reply spesific message, and use command",
     languages: "Python",
     url: "https://github.com/Hansennico/Discord-Bot",
-  },
-  {
-    name: "Dummy Project",
-    image: dummy,
-    description: "Lorem Ipsum",
-    languages: "",
-    url: "",
-  },
-  {
-    name: "Dummy Project",
-    image: dummy,
-    description: "Lorem Ipsum",
-    languages: "",
-    url: "",
-  },
-  {
-    name: "Dummy Project",
-    image: dummy,
-    description: "Lorem Ipsum",
-    languages: "",
-    url: "",
-  },
-  {
-    name: "Dummy Project",
-    image: dummy,
-    description: "Lorem Ipsum",
-    languages: "",
-    url: "",
-  },
-  {
-    name: "Dummy Project",
-    image: dummy,
-    description: "Lorem Ipsum",
-    languages: "",
-    url: "",
-  },
-  {
-    name: "Dummy Project",
-    image: dummy,
-    description: "Lorem Ipsum",
-    languages: "",
-    url: "",
   },
   {
     name: "Dummy Project",
